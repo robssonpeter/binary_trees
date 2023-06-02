70b6ed9c99ba7462e43ff0bf9f841c0755b9f531
Peter Mgembe <peterrobsson@gmail.com>
2023-06-02 08:16:37 -0700

    trying to fix betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,9 +2,11 @@
 
 /**
  * binary_treee_insert_left  - the function
- * description: a function to set the left item of a node
+ *
+ * Description: a function to set the left item of a node
  * @parent: Pointer to the parent node
  * @value: value to be set
+ * Return: binarty_tree_t variable
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
